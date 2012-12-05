82b2519e5744644e2ffe63f6744ab4b304d13fba
chintohere <kishorechintoju@gmail.com>
2012-12-05 18:11:35 +1100

    Update read-file.sh
    Script working

--- read-file.sh
+++ read-file.sh
@@ -1,1 +1,36 @@
-#read file
+#!/bin/sh
+
+#Initialize where all the sources will be exported to
+sources="sources"
+
+if [ -n "$1" ]; then
+        sources=$1
+fi
+
+echo "Code will be exported to '"$sources"'."
+
+#Read releases to export
+input=export.releases
+echo "Reading "$input
+
+#Repository tags base on SVN
+repo_tags="http://integ:90/repos/RASP/tags/GA/"
+
+#Start processing
+while read line
+do
+        release="$repo_tags$line"
+        if [ -n "$line" ]; then
+                dest="$sources/$line"
+                echo "Exporting : "$release" to dir: "$dest"..."
+                svn  export -q $release $dest
+                echo "Done."
+        fi
+done < "$input"
+echo "Export complete!"
+
+#Tar and zip them sources up
+echo "Packaging "$sources"..."
+tar czf "$sources.tar.gz" $sources
+echo "Done."
+                                                   
