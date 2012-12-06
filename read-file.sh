233537f2b2ee0c2951e44fd79cd573877245d952
chintohere <kishorechintoju@gmail.com>
2012-12-06 12:49:26 +1100

    Update read-file.sh

--- read-file.sh
+++ read-file.sh
@@ -1,36 +1,47 @@
-#!/bin/sh
+#Springboard
+GA_RASP_6.7.1_2012112
+GA_RASP_CORE_2.6.5_20121023
+GA_DATA_IMPORT_1.8_20121023
+GA_RASP_COMPONENT_1.4_20120806
+GA_RASP_ACTION_1.1.1_20110117
+jspa-persistence-2.4.0_20111026
 
-#Initialize where all the sources will be exported to
-sources="sources"
+#Webservices
+GA_rasp-ws-ear-6.0.0-20110726
+jspa-peristence-2.3.6_20101201
 
-if [ -n "$1" ]; then
-        sources=$1
-fi
+#RASP 5
+BR_RASP_5.4.3_20090602
+GA_RASP_UTIL_2.2.0_20090401
 
-echo "Code will be exported to '"$sources"'."
+#Rules Engine
+GA_RULES_1.1.0_20110208
+GA_RASP_CORE_2.3.2_20110207
+GA_RASP_ACTION_1.1.1_20110117
 
-#Read releases to export
-input=export.releases
-echo "Reading "$input
+#PDF Tool
+GA_BULKPRINT_1.1.0_20120119
 
-#Repository tags base on SVN
-repo_tags="http://integ:90/repos/RASP/tags/GA/"
+#Linked In Webservice
+linkedin-integration_1.0.3_20120521
 
-#Start processing
-while read line
-do
-        release="$repo_tags$line"
-        if [ -n "$line" ]; then
-                dest="$sources/$line"
-                echo "Exporting : "$release" to dir: "$dest"..."
-                svn  export -q $release $dest
-                echo "Done."
-        fi
-done < "$input"
-echo "Export complete!"
+#SMS Services
+GA_SMS_SERVICES_1.1.0_20121022
 
-#Tar and zip them sources up
-echo "Packaging "$sources"..."
-tar czf "$sources.tar.gz" $sources
-echo "Done."
-                                                   
+#Orbeon Integration
+GA_ORBEON_INTEGRATION_2.3.3_20120907
+GA_ORBEON_PARSER_1.0.0_20120907
+
+#Rasp Core Latest Release
+GA_RASP_CORE_3.6.6_20121203
+
+#Data Import Latest Release
+GA_DATA_IMPORT_1.9_20121203
+
+#JSPA - DAO Persistence Latest Release
+jspa-persistence_2.4.2_20120529  
+
+
+
+
+
